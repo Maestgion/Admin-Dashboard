b695790b941f1084d8b838e233011a090e2cf27f
Abhishek Dubey <7abhishek1410@gmial.com>
2022-03-13 16:43:45 +0530

    Navbar completed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 const Home = () => {
   return (
     <>
       <div className="home">
         <Sidebar />
-        <div className="homeContainer">container</div>
+        <div className="homeContainer">
+          <Navbar />
+        </div>
       </div>
     </>
   );
